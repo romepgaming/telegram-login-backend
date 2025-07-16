14fa2f68216442020af80af133034780a939f1a3
romepgaming <romep.gaming@gmail.com>
2025-07-16 17:16:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,25 +4,23 @@ from telethon.sessions import StringSession
 from flask_cors import CORS
 import os
 import asyncio
-import nest_asyncio  # ← WAJIB
-nest_asyncio.apply()  # ← WAJIB
+import nest_asyncio
+nest_asyncio.apply()
 
-# API ID dan HASH
 API_ID = 28723467
 API_HASH = "f51ed0208b47cfd04dc6409d64aa5bef"
 
-# Buat folder sesi
 SESSION_DIR = "session"
 if not os.path.exists(SESSION_DIR):
     os.makedirs(SESSION_DIR)
 
-# Setup Flask
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
 def home():
     return "✅ Telegram Login Backend Aktif"
+
 @app.route("/send_code", methods=["POST"])
 def send_code():
     data = request.json
@@ -54,7 +52,6 @@ def verify_code():
 
     async def verify():
         async with TelegramClient(f"{SESSION_DIR}/{phone}", API_ID, API_HASH) as client:
-            await client.connect()
             await client.sign_in(phone=phone, code=code)
             session_str = StringSession.save(client.session)
         return {"status": "logged_in", "session": session_str}
@@ -75,7 +72,6 @@ def verify_password():
 
     async def verify():
         async with TelegramClient(f"{SESSION_DIR}/{phone}", API_ID, API_HASH) as client:
-            await client.connect()
             await client.sign_in(password=password)
             session_str = StringSession.save(client.session)
         return {"status": "logged_in", "session": session_str}
